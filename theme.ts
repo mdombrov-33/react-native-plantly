46abe35b8b4b269a9a9dd5b9f457b242ae99113e
Maksym Dombrov <maksymdombrov@gmail.com>
2025-08-20 16:56:02 +0300

    linear gradient

--- theme.ts
+++ theme.ts
@@ -1,5 +1,7 @@
 export const theme = {
   colorGreen: '#29b365',
   colorWhite: '#fff',
-  colorLeafyGreen: '206a42'
+  colorLeafyGreen: '#206a42',
+  colorAppleGreen: '#8db600',
+  colorLimeGreen: '#d0e57e'
 };
