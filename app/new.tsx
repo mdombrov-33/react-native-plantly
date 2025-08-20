1b8ad048bba9dcc6a26eb790e39aa1068735502f
Maksym Dombrov <maksymdombrov@gmail.com>
2025-08-20 21:37:49 +0300

    image on home page

--- app/new.tsx
+++ app/new.tsx
@@ -41,7 +41,7 @@ export default function NewScreen() {
       );
     }
 
-    addPlant(name, Number(days));
+    addPlant(name, Number(days), imageUri);
     router.navigate('/');
 
     console.log('Adding plant', name, days);
