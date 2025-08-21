663a7326afc1b2d90675091db8ae28e844c04dce
Maksym Dombrov <maksymdombrov@gmail.com>
2025-08-21 19:03:23 +0300

    deep link with parameters

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -3,6 +3,11 @@ import { AntDesign } from '@expo/vector-icons';
 import { Link, Stack } from 'expo-router';
 import { Pressable } from 'react-native';
 
+//* make sure that if we're not rendering index, to make index render underneath anyway
+export const unstable_settings = {
+  initialRouteName: 'index'
+};
+
 export default function Layout() {
   return (
     <Stack>
