cfca1fcd22120e5d68626b95c570330a1d058456
Maksym Dombrov <maksymdombrov@gmail.com>
2025-08-21 18:43:30 +0300

    add quick actions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,8 @@
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import * as QuickActions from 'expo-quick-actions';
+import { Platform } from 'react-native';
+import { useQuickActionRouting } from 'expo-quick-actions/router';
 
 export default function Layout() {
   //* Lock the splash screen
@@ -6,6 +10,25 @@ export default function Layout() {
   //* Hide the splash screen
   // SplashScreen.hideAsync();
   //* Could be useful in auth scenarios where we want to check some info, and when its ready we can hide the splash screen
+
+  //* use it when we pass params into quick actions like below
+  //* to automatically link to the page that is specified in href
+  useQuickActionRouting();
+
+  const setQuickActions = () => {
+    QuickActions.setItems([
+      {
+        title: 'Add a plant',
+        icon: Platform.OS === 'ios' ? 'symbol:leaf' : 'leaf', //* leaf is the name in app.json
+        id: '0',
+        params: { href: '/new' }
+      }
+    ]);
+  };
+
+  useEffect(() => {
+    setQuickActions();
+  }, []);
 
   return (
     <Stack>
---
+++ write-local-properties.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#! chmod +x ./write-local-properties.sh
+echo "sdk.dir=/home/maximator16/Android/SdkLinux" > ./android/local.properties
