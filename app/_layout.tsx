1e9787aadfdd9e44c90ec70486584ab1c12ecb59
Maksym Dombrov <maksymdombrov@gmail.com>
2025-08-21 17:13:44 +0300

    add comments

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
+  //* Lock the splash screen
+  // SplashScreen.preventAutoHideAsync();
+  //* Hide the splash screen
+  // SplashScreen.hideAsync();
+  //* Could be useful in auth scenarios where we want to check some info, and when its ready we can hide the splash screen
+
   return (
     <Stack>
       <Stack.Screen
